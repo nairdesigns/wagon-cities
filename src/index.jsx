84b72ea2677ea2586c4049722909d749ebfd4854
nairdesigns <nairbdg@gmail.com>
2022-04-18 21:15:21 +0800

    yarn start finally working dependincies fixed

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
 // external modules
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers } from "redux";
 
 // internal modules
-import App from './components/app';
-import '../assets/stylesheets/application.scss';
+import App from "./components/app";
+import "./application.css";
 
 // State and reducers
 const reducers = combineReducers({
-  changeMe: (state = null, action) => state
+  changeMe: (state = null, action) => state,
 });
 
 // render an instance of the component in the DOM
@@ -18,5 +18,5 @@ ReactDOM.render(
   <Provider store={createStore(reducers)}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
