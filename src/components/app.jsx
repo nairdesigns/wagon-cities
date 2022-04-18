5f80e9a2f58c73d0b77f7f8e3cef75fd4ed72caa
nairdesigns <nairbdg@gmail.com>
2022-04-18 20:52:53 +0800

    changed dependencies

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+
+const cities = [
+  { name: "Paris", address: "16 Villa Gaudelet, 75011 Paris", slug: "paris" },
+  { name: "London", address: "14-22 Elder St, London E1 6BT", slug: "london" },
+  {
+    name: "Berlin",
+    address: "Rudi-Dutschke-Straße 26, 10969 Berlin",
+    slug: "berlin",
+  },
+];
 
 const App = () => {
   return (
     <div className="app">
-      <p>React + Redux starter</p>
+      <CityList cities={cities} />
     </div>
   );
 };
-
 export default App;
