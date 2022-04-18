196eff6624242a9b3589375cfe9904103cba291e
nairdesigns <nairbdg@gmail.com>
2022-04-18 23:55:17 +0800

    cities working delted vs file

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,15 +1,6 @@
 import React, { Component } from "react";
 import CityList from "../containers/city_list";
-
-const cities = [
-  { name: "Paris", address: "16 Villa Gaudelet, 75011 Paris", slug: "paris" },
-  { name: "London", address: "14-22 Elder St, London E1 6BT", slug: "london" },
-  {
-    name: "Berlin",
-    address: "Rudi-Dutschke-Straße 26, 10969 Berlin",
-    slug: "berlin",
-  },
-];
+import cities from "../data/cities";
 
 export default class App extends Component {
   constructor(props) {
